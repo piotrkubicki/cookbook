1bc0897ed5f7ce0b1d591f4474fa1f15ba08c585
Piotr <pkubicki44@gmail.com>
2016-01-02 00:09:21 +0000

    recipe display added

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,9 +3,29 @@ angular.module('main', ['routes', 'fileService'])
 
 .controller('mainController', function(fileReader) {
   var self = this;
-  // get recipes titles
-  fileReader.readFile('index.txt').then(function(res){
-    self.recipes = res.toString().split('\n');;
-  });
 
+  // used to retrive all recipes titles from index.txt file
+  var getRecipesTitles = function() {
+    // get recipes titles
+    fileReader.readFile('index.txt').then(function(res) {
+      self.recipes = res.toString().split('\n');
+    }, function(reason) {
+      console.log(reason);
+    });
+  }
+
+  // used to retrive whole recipe
+  self.getRecipe = function(recipe) {
+    var recipe = recipe.replace('\n', '').trim(0, -1) + '.txt';
+
+    console.log(recipe[recipe.length - 1]);
+    fileReader.readFile(recipe).then(function(res) {
+      self.recipe = res.toString();
+    }, function(reason) {
+      console.log(reason);
+    });
+  }
+
+  // MAIN
+  getRecipesTitles();
 });
